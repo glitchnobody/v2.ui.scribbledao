2f4b7553c47c7ac051d12a6d50f076fe507438c7
glitchnobody <58382686+glitchnobody@users.noreply.github.com>
2025-05-13 19:49:09 +0530

    first

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Figtree, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const figSans = Figtree({
+  variable: "--font-fig-sans",
+  subsets: ["latin"],
+  fallback: ["sans-serif"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  fallback: ["monospace"],
+});
+
+export const metadata: Metadata = {
+  title: "Scribble Dao",
+  description: "Earn Crypto for your Efforts!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${figSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
